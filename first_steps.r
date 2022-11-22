8eb81b47671085f846baed2be1ce556b497e5ec0
Bruno Urbán <brurban99@gmail.com>
2022-11-22 11:11:54 +0100

    Create first_steps.r

---
+++ first_steps.r
@@ -0,0 +1,12 @@
+#comment
+first_variable <- "This is my variable"
+second_variable <- 12.5
+vec_1 <- c(1, 2, 3, 4)
+list_1 <- list("a", 1L, 1.5, TRUE)
+data.frame(x = c(1, 2, 3) , y = c(1.5, 5.5, 7.5))
+dir.create ("destination_folder")
+
+file.create("new_csv_file.csv") 
+file.copy ("new_csv_file.csv" , "destination_folder")
+matrix(c(3:8), nrow = 2)
+matrix(c(3:8), ncol = 2)
